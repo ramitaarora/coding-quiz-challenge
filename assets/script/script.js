36996faffc9d61ffd6dd93a17080301a1a161694
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-09-12 16:26:22 -0700

    added quizData object and defined HTML elements

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+var totalWins;
+var totalLosses;
+
+quizData = [
+    {
+        question: "Commonly used data types do NOT include:",
+        answers: ["alerts", "strings", "booleans", "numbers"] 
+    },
+    {
+        question: "The condition in an if/else state is enclosed with:",
+        answer: ["curly brackets", "quotes", "parenthesis", "square brackets"]
+    },
+    {
+        question: "Arrays in Javascript can be used to store:",
+        answer: ["all of the above", "strings", "numbers", "other arrays"]
+    },
+    {
+        question: "Fill in the blank: string values must be enclosed with _____ when being assigned to variables.",
+        answer: ["quotes", "curly brackets", "commas", "parenthesis"]
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: ["console.log", "JavaScript", "terminal/bash", "for loops"]
+    }
+]
+
