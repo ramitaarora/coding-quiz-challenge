ed1655192f2b9e617fa085ed67b632de86b1199b
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-09-12 17:22:39 -0700

    added event listener to startButton; created playQuiz function that is called when start button is clicked; created function nextQuestion that writes the question to the page

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,7 +4,7 @@ var totalLosses;
 quizData = [
     {
         question: "Commonly used data types do NOT include:",
-        answers: ["alerts", "strings", "booleans", "numbers"] 
+        answer: ["alerts", "strings", "booleans", "numbers"] 
     },
     {
         question: "The condition in an if/else state is enclosed with:",
@@ -25,9 +25,32 @@ quizData = [
 ]
 
 var startButton = document.querySelector("#start-button");
-var quizVisibility = document.querySelector(".quiz");
+var quiz = document.querySelector(".quiz");
+var question = document.querySelector(".question");
+var answers = document.querySelector(".answers");
 
 startButton.addEventListener('click', function() {
-    quizVisibility.removeAttribute("class", "hidden");
-    startButton.setAttribute("class", "hidden")
+    quiz.removeAttribute("class", "hidden");
+    startButton.setAttribute("class", "hidden");
+    playQuiz();
 })
+
+function playQuiz() {
+    var questionNumber = 0;
+    nextQuestion([questionNumber]);
+
+    function nextQuestion() {
+        var eachQuestion = document.createElement("h2");
+        eachQuestion.textContent = quizData[questionNumber].question;
+        question.append(eachQuestion);
+
+        for (var i=0; i < (quizData[questionNumber].answer).length; i++) {
+            var eachAnswer = document.createElement("p");
+            eachAnswer.textContent = quizData[questionNumber].answer[i]
+            answers.append(eachAnswer);
+        }
+    }
+    
+
+    
+}
